56307cf29bb4897dc26f49c94b0699a97e6a082b
Tom McClintock <tmcclintock89@gmail.com>
2020-08-10 10:32:56 -0700

    Tested email stuff in the dag. Is being annoying.

--- airflow/dags/simple_dag.py
+++ airflow/dags/simple_dag.py
@@ -4,26 +4,34 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 import sys
 
+# this function and the one below are from this page:
+# https://forum.astronomer.io/t/can-i-get-email-notifications-when-tasks-succeed/408
+        
 def print_hello():
-    return f"Oy lad I'll hook ya gabba! {sys.version}"
+    s =f"Oy lad I'll hook ya gabba! {sys.version}"
+    print(s + " inside here!")  # This does not get printed w `airflow test...`
+    # raise Exception("testing failures")
+    return s
 
 default_args = {
-    "owner": "airflow",
+"owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2020, 8, 7),
-    "email": ["thmsmcclintock@gmail.com"],
-    "email_on_failure": False,
+    "start_date": datetime(2020, 6, 6),
+    "email_on_failure": True,
     "email_on_retry": False,
+    #"email": "thmsmcclintock@gmail.com",
+    #"on_success_callback": task_success_callback,
     "retries": 1,
     "retry_delay": timedelta(minutes=2),
 }
 
+# Note - if the dag_id changes you need to rerun `airflow scheduler`
 dag = DAG(
-    "hello_lad",
-    description="My tutorial DAG",
-    schedule_interval="04 * * * *",
-    default_args=default_args,
-    catchup=False,
+        dag_id="example",
+        description="My tutorial DAG",
+        schedule_interval="30 * * * *",
+        default_args=default_args,
+        catchup=False,
 )
 
 t1 = DummyOperator(task_id="dummy_task", retries=3, dag=dag)

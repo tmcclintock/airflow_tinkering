4a106b6959847f9db3f985abcdfd5d977a50936f
Tom McClintock <tmcclintock89@gmail.com>
2020-08-28 16:46:43 -0700

    Wrote a simple dag.

--- airflow/dags/simple_dag.py
+++ airflow/dags/simple_dag.py
@@ -3,48 +3,24 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.email import send_email_smtp
-import sys
+import os, sys
 
-# note: one needs to set up an 'app password' for the email
-# address that will be doing the mailing. See this page:
-# https://stackoverflow.com/questions/51829200/how-to-set-up-airflow-send-email
-
-# this function and the one below are from this page:
-# https://forum.astronomer.io/t/can-i-get-email-notifications-when-tasks-succeed/408
-def task_success_callback(context):
-	outer_task_success_callback(context, email='destination@email.com')
-
-
-def outer_task_success_callback(context, email):
-	subject = "[Airflow] DAG {0} - Task {1}: Success".format(
-		context['task_instance_key_str'].split('__')[0],
-		context['task_instance_key_str'].split('__')[1]
-		)
-	html_content = """
-	DAG: {0}<br>
-	Task: {1}<br>
-	Succeeded on: {2}
-	""".format(
-		context['task_instance_key_str'].split('__')[0],
-		context['task_instance_key_str'].split('__')[1],
-		datetime.now()
-		)
-	send_email_smtp(email, subject, html_content)
         
 def print_hello():
     s =f"Oy lad I'll hook ya gabba! {sys.version}"
     print(s + " inside here!")  # This does not get printed w `airflow test...`
     # raise Exception("testing failures")
+    body = "this is the body"
+    header = "this is the header"
+    address = "thmsmcclintock@gmail.com"
+    mail_cmd = f"echo \"{body}\" | mail -s \"{header}\" {address}"
+    os.system(mail_cmd)
     return s
 
 default_args = {
 "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2020, 6, 6),
-    "email_on_failure": True,
-    "email_on_retry": False,
-    "email": "thmsmcclintock@gmail.com",
-    "on_success_callback": task_success_callback,
     "retries": 1,
     "retry_delay": timedelta(minutes=2),
 }
@@ -53,7 +29,7 @@ default_args = {
 dag = DAG(
         dag_id="example",
         description="My tutorial DAG",
-        schedule_interval="30 * * * *",
+        schedule_interval="11 * * * *",
         default_args=default_args,
         catchup=False,
 )
